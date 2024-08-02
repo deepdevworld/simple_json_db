8861a4196ac4753c9e5d95fd2da04420b9f22b5c
sushil <sushilrai@qikpod.com>
2024-08-02 16:21:48 +0530

    removed main.py and solved import issue.

--- main.py
+++
@@ -1,9 +0,0 @@
-from json_db import JsonDB
-
-
-# db = JsonDB(db_name="demo.json")
-# # db.add_table("user")
-# db.add("user", {"name": "rai"})
-# print(db.get_by_id())
-# print(db.get_by_query(table_name="user", query={"name": "alex"}))
-# db.delete_by_id("user", pk=4)
